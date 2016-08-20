7d7845019dadba656511650d2859a1a533adeb72
Hung Hoang <Hung Hoang>
2016-08-20 12:03:31 +0100

    Player movement is fine. needs tuning for speed etc.

--- Assets/Scripts/Controls/MovementController.cs
+++ Assets/Scripts/Controls/MovementController.cs
@@ -5,31 +5,55 @@ public class MovementController : MonoBehaviour
 {
 
     public float Speed = 0.1f;
+    Rigidbody rb;
 
     private Vector3 moveForce;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     void Start()
     {
         moveForce = new Vector3(Speed, Speed, Speed);
+        rb.freezeRotation = true;
+        rb.drag = 4.0f;
     }
 
     void Update()
     {
+        // will need to take care of movement if hit zombie
+
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position += transform.TransformDirection(Vector3.Scale(Vector3.forward, moveForce));
+            rb.AddForce(transform.TransformDirection(Vector3.Scale(Vector3.forward, moveForce)));
         }
         if (Input.GetKey(KeyCode.S))
         {
-			transform.position += transform.TransformDirection(Vector3.Scale(Vector3.back, moveForce));
+            rb.AddForce(transform.TransformDirection(Vector3.Scale(Vector3.back, moveForce)));
         }
         if (Input.GetKey(KeyCode.A))
         {
-			transform.position += transform.TransformDirection(Vector3.Scale(Vector3.left, moveForce));
+            rb.AddForce(transform.TransformDirection(Vector3.Scale(Vector3.left, moveForce)));
         }
         if (Input.GetKey(KeyCode.D))
         {
-			transform.position += transform.TransformDirection(Vector3.Scale(Vector3.right, moveForce));
+            rb.AddForce(transform.TransformDirection(Vector3.Scale(Vector3.right, moveForce)));
+        }
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Wall"))
+        {
+            Debug.Log("hit wall");
+            rb.velocity = Vector3.zero;
         }
     }
 }
